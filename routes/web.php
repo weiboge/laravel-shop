f5092a1cf620bb6e97dd05ebf75791afa94d9c25
宋天耀 <309833997@qq.com>
2019-07-07 07:07:00 +0000

    收货地址列表

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 Route::get('/', 'PagesController@root')->name('root');
 
-Auth::routes();
-
+// 在之前的路由里加上一个 verify 参数
+Auth::routes(['verify' => true]);
 //Route::get('/home', 'HomeController@index')->name('home');
+
+// auth 中间件代表需要登录，verified中间件代表需要经过邮箱验证
+Route::group(['middleware' => ['auth', 'verified']], function() {
+    Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
+});
