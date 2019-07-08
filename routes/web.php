7e82382b1c131bfdeb76eb0ba2942ef243b50e46
宋天耀 <309833997@qq.com>
2019-07-08 02:28:25 +0000

    用户界面-商品列表

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
-Route::get('/', 'PagesController@root')->name('root');
+Route::redirect('/', '/products')->name('root');
+Route::get('products', 'ProductsController@index')->name('products.index');
 
 // 在之前的路由里加上一个 verify 参数
 Auth::routes(['verify' => true]);
