fea9ed17c8ccd1e85b45c64c8a9c03b90f66b954
宋天耀 <309833997@qq.com>
2019-07-08 08:08:19 +0000

    管理后台 - 新增、修改、删除优惠券

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -16,4 +16,10 @@ Route::group([
     $router->post('products', 'ProductsController@store');
     $router->get('products/{id}/edit', 'ProductsController@edit');
     $router->put('products/{id}', 'ProductsController@update');
+    $router->get('coupon_codes', 'CouponCodesController@index');
+    $router->post('coupon_codes', 'CouponCodesController@store');
+    $router->get('coupon_codes/create', 'CouponCodesController@create');
+    $router->get('coupon_codes/{id}/edit', 'CouponCodesController@edit');
+    $router->put('coupon_codes/{id}', 'CouponCodesController@update');
+    $router->delete('coupon_codes/{id}', 'CouponCodesController@destroy');
 });
