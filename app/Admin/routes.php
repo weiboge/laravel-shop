3536e97ae2db974a0d1a65d2d292b78ce130e416
宋天耀 <309833997@qq.com>
2019-07-09 02:51:07 +0000

    管理后台 - 订单发货

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -24,4 +24,5 @@ Route::group([
     $router->delete('coupon_codes/{id}', 'CouponCodesController@destroy');
     $router->get('orders', 'OrdersController@index')->name('admin.orders.index');
     $router->get('orders/{order}', 'OrdersController@show')->name('admin.orders.show');
+    $router->post('orders/{order}/ship', 'OrdersController@ship')->name('admin.orders.ship');
 });
