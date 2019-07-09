f68c22b102c68126247b00f5e4afedb87dda3bda
宋天耀 <309833997@qq.com>
2019-07-09 01:39:12 +0000

    订单支付

--- routes/web.php
+++ routes/web.php
@@ -27,10 +27,12 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::get('orders', 'OrdersController@index')->name('orders.index');
     Route::get('orders/{order}', 'OrdersController@show')->name('orders.show');
     Route::get('coupon_codes/{code}', 'CouponCodesController@show')->name('coupon_codes.show');
+    Route::get('payment/{order}/alipay', 'PaymentController@payByAlipay')->name('payment.alipay');
+    Route::get('payment/alipay/return', 'PaymentController@alipayReturn')->name('payment.alipay.return');//前端回调
 
 });
 Route::get('products/{product}', 'ProductsController@show')->name('products.show');
-
+Route::post('payment/alipay/notify', 'PaymentController@alipayNotify')->name('payment.alipay.notify');//服务器端回调
 Route::get('alipay', function() {
     return app('alipay')->web([
         'out_trade_no' => time(),
