ae16f3020e5d08a8c4049bfc30e462708510f6c2
宋天耀 <309833997@qq.com>
2019-07-09 03:39:17 +0000

    评价商品

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -7,6 +7,10 @@ use App\Models\Order;
 use Illuminate\Http\Request;
 use App\Services\OrderService;
 use App\Exceptions\InvalidRequestException;
+use Carbon\Carbon;
+use App\Http\Requests\SendReviewRequest;
+use App\Events\OrderReviewed;
+
 class OrdersController extends Controller
 {
     public function store(OrderRequest $request, OrderService $orderService)
@@ -31,20 +35,47 @@ class OrdersController extends Controller
     {
         return view('orders.show', ['order' => $order->load(['items.productSku', 'items.product'])]);
     }
-    public function received(Order $order, Request $request)
+    public function review(Order $order)
     {
         // 校验权限
         $this->authorize('own', $order);
-
-        // 判断订单的发货状态是否为已发货
-        if ($order->ship_status !== Order::SHIP_STATUS_DELIVERED) {
-            throw new InvalidRequestException('发货状态不正确');
+        // 判断是否已经支付
+        if (!$order->paid_at) {
+            throw new InvalidRequestException('该订单未支付，不可评价');
         }
+        // 使用 load 方法加载关联数据，避免 N + 1 性能问题
+        return view('orders.review', ['order' => $order->load(['items.productSku', 'items.product'])]);
+    }
 
-        // 更新发货状态为已收到
-        $order->update(['ship_status' => Order::SHIP_STATUS_RECEIVED]);
+    public function sendReview(Order $order, SendReviewRequest $request)
+    {
+        // 校验权限
+        $this->authorize('own', $order);
+        if (!$order->paid_at) {
+            throw new InvalidRequestException('该订单未支付，不可评价');
+        }
+        // 判断是否已经评价
+        if ($order->reviewed) {
+            throw new InvalidRequestException('该订单已评价，不可重复提交');
+        }
+        $reviews = $request->input('reviews');
+        // 开启事务
+        \DB::transaction(function () use ($reviews, $order) {
+            // 遍历用户提交的数据
+            foreach ($reviews as $review) {
+                $orderItem = $order->items()->find($review['id']);
+                // 保存评分和评价
+                $orderItem->update([
+                    'rating'      => $review['rating'],
+                    'review'      => $review['review'],
+                    'reviewed_at' => Carbon::now(),
+                    event(new OrderReviewed($order))
+                ]);
+            }
+            // 将订单标记为已评价
+            $order->update(['reviewed' => true]);
+        });
 
-        // 返回订单信息
-        return $order;
+        return redirect()->back();
     }
 }
